733fde4b171478e5b8845bd3b5e6b2de5c83bdf1
CDR4733 <shsing2@naver.com>
2026-01-10 08:51:14 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ JavaScript/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,11 +1,8 @@
 function solution(array, height) {
     var answer = 0;
-    array.sort((a, b) => b - a)
     for (let i = 0; i < array.length; i++) {
-        if (array[i] > height) {
-            answer++
-        } else { 
-            break;
+        if (height < array[i]) {
+            answer++;
         }
     }
     return answer;
