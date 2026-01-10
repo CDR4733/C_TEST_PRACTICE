84c89a60968de07c77398621dbc209eea80cad1c
CDR4733 <shsing2@naver.com>
2026-01-10 23:05:40 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    var answer = num_list;
+    if (num_list.at(-1) > num_list.at(-2)) {
+        answer.push(num_list.at(-1) - num_list.at(-2));
+    } else {
+        answer.push(num_list.at(-1) * 2);
+    }
+    return answer;
+}
