1edb12f4287779d45936cbb05f610172f44b3f8e
CDR4733 <shsing2@naver.com>
2026-01-10 12:24:09 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ JavaScript/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,11 +1,7 @@
 function solution(n) {
     var answer = [];
-    for(let i = 0; i < n; i++) {
-        if (2 * i + 1 <= n) {
-            answer.push(2 * i + 1);
-        } else {
-            break;
-        }
+    for (let i = 0; (2 * i + 1) <= n; i++) {
+        answer.push(2 * i + 1);
     }
     return answer;
 }
