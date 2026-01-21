fd1bfb8f3a9297f3e65041f95dc17de33c3b3c4f
CDR4733 <shsing2@naver.com>
2026-01-22 04:27:00 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ JavaScript/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,5 @@
-function solution(s1, s2) {
-    var answer = 0;
-    for (let i = 0; i < s1.length; i++) {
-        for (let j = 0; j < s2.length; j++) {
-            if (s1[i] == s2[j]) {
-                answer++;
-            }
-        }
-    }
+function solution(s1, s2) {    
+    var intersection = new Set([...s1, ...s2]).size;
+    var answer = s1.length + s2.length - intersection;
     return answer;
 }
